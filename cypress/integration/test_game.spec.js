42801d6891523e34be555d32ba45aa0e5f565168
Srivastava <Ankit.Srivastava@emc.com>
2019-06-19 07:39:12 +0530

    Clean up test_game.spec.js

--- cypress/integration/test_game.spec.js
+++ cypress/integration/test_game.spec.js
@@ -29,17 +29,12 @@ describe('Launch slot game application test',() => {
         cy.get('#testdata').type('11111')
         cy.get('#spinButton').click()
         
-        if (cy.get('#reel1 > .notch2').contains('1') & 
-            cy.get('#reel2 > .notch2').contains('1') & 
-            cy.get('#reel3 > .notch2').contains('1') &
-            cy.get('#reel4 > .notch2').contains('1') &
-            cy.get('#reel5 > .notch2').contains('1') &
-            cy.contains('Win 100 coins').should('be.visible') ){
-            console.log('Checked the presence................')
-        }
-        else{
-            console.log('Wrong highlight ............')
-        }        
+        cy.get('#reel1 > .notch2').contains('1') & 
+        cy.get('#reel2 > .notch2').contains('1') & 
+        cy.get('#reel3 > .notch2').contains('1') &
+        cy.get('#reel4 > .notch2').contains('1') &
+        cy.get('#reel5 > .notch2').contains('1') &
+        cy.contains('Win 100 coins').should('be.visible') 
                         
     })   
 
@@ -76,17 +71,11 @@ describe('Launch slot game application test',() => {
         cy.get('#testdata').type('111')
         cy.wait(5000)
         cy.get('#spinButton').click()
-            if (cy.get('#reel1 > .notch2').contains('1') & 
-            cy.get('#reel2 > .notch2').contains('1') & 
-            cy.get('#reel3 > .notch2').contains('1') &                          
-            cy.contains('Win 60 coins').should('be.visible') ){
-        console.log('Checked the presence................')
-    }
-    else{
-        console.log('Wrong highlight ............')
-    } 
+        cy.get('#reel1 > .notch2').contains('1') & 
+        cy.get('#reel2 > .notch2').contains('1') & 
+        cy.get('#reel3 > .notch2').contains('1') &                          
+        cy.contains('Win 60 coins').should('be.visible')
         
-                
     })
     it ('Check if click with test data 1111 adds up 80 to total present score always',() => {
 
@@ -94,18 +83,11 @@ describe('Launch slot game application test',() => {
         cy.get('#testdata').type('1111')
         cy.wait(5000)
         cy.get('#spinButton').click()
-            if (cy.get('#reel1 > .notch2').contains('1') & 
-            cy.get('#reel2 > .notch2').contains('1') & 
-            cy.get('#reel3 > .notch2').contains('1') &
-            cy.get('#reel4 > .notch2').contains('1') &                  
-            cy.contains('Win 80 coins').should('be.visible') ){
-        console.log('Checked the presence................')
-    }
-    else{
-        console.log('Wrong highlight ............')
-    }        
-    
-        
+        cy.get('#reel1 > .notch2').contains('1') & 
+        cy.get('#reel2 > .notch2').contains('1') & 
+        cy.get('#reel3 > .notch2').contains('1') &
+        cy.get('#reel4 > .notch2').contains('1') &                  
+        cy.contains('Win 80 coins').should('be.visible')        
                 
     })
     it ('Check if click with test data 11111 adds up 100 to total present score always',() => {
@@ -114,54 +96,33 @@ describe('Launch slot game application test',() => {
         cy.get('#testdata').type('11111')
         cy.wait(5000)
         cy.get('#spinButton').click()
-            if (cy.get('#reel1 > .notch2').contains('1') & 
-            cy.get('#reel2 > .notch2').contains('1') & 
-            cy.get('#reel3 > .notch2').contains('1') &
-            cy.get('#reel4 > .notch2').contains('1') &
-            cy.get('#reel5 > .notch2').contains('1') &           
-            cy.contains('Win 100 coins').should('be.visible') ){
-        console.log('Checked the presence................')
-    }
-    else{
-        console.log('Wrong highlight ............')
-}        
-        
-                
+        cy.get('#reel1 > .notch2').contains('1') & 
+        cy.get('#reel2 > .notch2').contains('1') & 
+        cy.get('#reel3 > .notch2').contains('1') &
+        cy.get('#reel4 > .notch2').contains('1') &
+        cy.get('#reel5 > .notch2').contains('1') &           
+        cy.contains('Win 100 coins').should('be.visible')                                  
     })
     it ('Check if click with test data 222 adds up 120 to total present score always',() => {
         cy.get('#testdata').clear()      
         cy.get('#testdata').type('222')
         cy.wait(5000)
         cy.get('#spinButton').click()
-            if (cy.get('#reel1 > .notch2').contains('2') & 
-            cy.get('#reel2 > .notch2').contains('2') & 
-            cy.get('#reel3 > .notch2').contains('2') &           
-            cy.contains('Win 120 coins').should('be.visible') ){
-        console.log('Checked the presence................')
-    }
-    else{
-        console.log('Wrong highlight ............')
-    }        
-                        
+        cy.get('#reel1 > .notch2').contains('2') & 
+        cy.get('#reel2 > .notch2').contains('2') & 
+        cy.get('#reel3 > .notch2').contains('2') &           
+        cy.contains('Win 120 coins').should('be.visible')                                     
     })
     it ('Check if click with test data 2222 adds up 160 to total present score always',() => {
         cy.get('#testdata').clear()      
         cy.get('#testdata').type('2222')
         cy.wait(5000)
         cy.get('#spinButton').click()
-        if (cy.get('#reel1 > .notch2').contains('2') & 
-            cy.get('#reel2 > .notch2').contains('2') & 
-            cy.get('#reel3 > .notch2').contains('2') &   
-            cy.get('#reel4 > .notch2').contains('2') &               
-            cy.contains('Win 160 coins').should('be.visible') ){
-            console.log('Checked the presence................')
-        }
-        else{
-            console.log('Wrong highlight ............')
-        }
-        
-        
-                
+        cy.get('#reel1 > .notch2').contains('2') & 
+        cy.get('#reel2 > .notch2').contains('2') & 
+        cy.get('#reel3 > .notch2').contains('2') &   
+        cy.get('#reel4 > .notch2').contains('2') &               
+        cy.contains('Win 160 coins').should('be.visible')                        
     })
     it ('Check if click with test data 22222 adds up 200 to total present score always',() => {
 
@@ -169,19 +130,12 @@ describe('Launch slot game application test',() => {
         cy.get('#testdata').type('22222')
         cy.wait(5000)
         cy.get('#spinButton').click()
-        if (cy.get('#reel1 > .notch2').contains('2') & 
-            cy.get('#reel2 > .notch2').contains('2') & 
-            cy.get('#reel3 > .notch2').contains('2') &   
-            cy.get('#reel4 > .notch2').contains('2') &   
-            cy.get('#reel5 > .notch2').contains('2') &                                        
-            cy.contains('Win 200 coins').should('be.visible') ){
-            console.log('Checked the presence................')
-        }
-        else{
-            console.log('Wrong highlight ............')
-        }
-        
-                
+        cy.get('#reel1 > .notch2').contains('2') & 
+        cy.get('#reel2 > .notch2').contains('2') & 
+        cy.get('#reel3 > .notch2').contains('2') &   
+        cy.get('#reel4 > .notch2').contains('2') &   
+        cy.get('#reel5 > .notch2').contains('2') &                                        
+        cy.contains('Win 200 coins').should('be.visible')                        
     })
     it ('Check if click with test data 333 adds up 180 to total present score always',() => {
 
@@ -189,18 +143,10 @@ describe('Launch slot game application test',() => {
         cy.get('#testdata').type('333')
         cy.wait(5000)
         cy.get('#spinButton').click()
-        if (cy.get('#reel1 > .notch2').contains('3') & 
-            cy.get('#reel2 > .notch2').contains('3') & 
-            cy.get('#reel3 > .notch2').contains('3') &                                        
-            cy.contains('Win 180 coins').should('be.visible') ){
-            console.log('Checked the presence................')
-        }
-        else{
-            console.log('Wrong highlight ............')
-        }
-        
-        
-                
+        cy.get('#reel1 > .notch2').contains('3') & 
+        cy.get('#reel2 > .notch2').contains('3') & 
+        cy.get('#reel3 > .notch2').contains('3') &                                        
+        cy.contains('Win 180 coins').should('be.visible')                
     })
     it ('Check if click with test data 3333 adds up 240 to total present score always',() => {
 
@@ -208,59 +154,33 @@ describe('Launch slot game application test',() => {
         cy.get('#testdata').type('3333')
         cy.wait(5000)
         cy.get('#spinButton').click()
-        if (cy.get('#reel1 > .notch2').contains('3') & 
-            cy.get('#reel2 > .notch2').contains('3') & 
-            cy.get('#reel3 > .notch2').contains('3') &
-            cy.get('#reel4 > .notch2').contains('3') &                                
-            cy.contains('Win 240 coins').should('be.visible') ){
-            console.log('Checked the presence................')
-        }
-        else{
-            console.log('Wrong highlight ............')
-        }
-        
-                
+        cy.get('#reel1 > .notch2').contains('3') & 
+        cy.get('#reel2 > .notch2').contains('3') & 
+        cy.get('#reel3 > .notch2').contains('3') &
+        cy.get('#reel4 > .notch2').contains('3') &                                
+        cy.contains('Win 240 coins').should('be.visible')                        
     })
-    it ('Check if click with test data 33333 adds up 300 to total present score always',() => {
-
-        
+    it ('Check if click with test data 33333 adds up 300 to total present score always',() => {        
         cy.get('#testdata').clear()      
         cy.get('#testdata').type('33333')
         cy.wait(5000)
         cy.get('#spinButton').click()
-        if (cy.get('#reel1 > .notch2').contains('3') & 
-            cy.get('#reel2 > .notch2').contains('3') & 
-            cy.get('#reel3 > .notch2').contains('3') &
-            cy.get('#reel4 > .notch2').contains('3') &
-            cy.get('#reel5 > .notch2').contains('3') &                    
-            cy.contains('Win 300 coins').should('be.visible') ){
-            console.log('Checked the presence................')
-        }
-        else{
-            console.log('Wrong highlight ............')
-        }
-        
-        
-                
-                
+        cy.get('#reel1 > .notch2').contains('3') & 
+        cy.get('#reel2 > .notch2').contains('3') & 
+        cy.get('#reel3 > .notch2').contains('3') &
+        cy.get('#reel4 > .notch2').contains('3') &
+        cy.get('#reel5 > .notch2').contains('3') &                    
+        cy.contains('Win 300 coins').should('be.visible')                                                
     })
     it ('Check if click with test data 444 adds up 240 to total present score always',() => {
         cy.get('#testdata').clear()      
         cy.get('#testdata').type('444')
         cy.wait(5000)
         cy.get('#spinButton').click()
-        if (cy.get('#reel1 > .notch2').contains('4') & 
-            cy.get('#reel2 > .notch2').contains('4') & 
-            cy.get('#reel3 > .notch2').contains('4') &                    
-            cy.contains('Win 240 coins').should('be.visible') ){
-            console.log('Checked the presence................')
-        }
-        else{
-            console.log('Wrong highlight ............')
-        }
-        
-        
-                
+        cy.get('#reel1 > .notch2').contains('4') & 
+        cy.get('#reel2 > .notch2').contains('4') & 
+        cy.get('#reel3 > .notch2').contains('4') &                    
+        cy.contains('Win 240 coins').should('be.visible')                
     })
     it ('Check if click with test data 4444 adds up 320 to total present score always',() => {
 
@@ -268,97 +188,56 @@ describe('Launch slot game application test',() => {
         cy.get('#testdata').type('4444')
         cy.wait(5000)
         cy.get('#spinButton').click()
-        if (cy.get('#reel1 > .notch2').contains('4') & 
-            cy.get('#reel2 > .notch2').contains('4') & 
-            cy.get('#reel3 > .notch2').contains('4') &
-            cy.get('#reel4 > .notch2').contains('4') &           
-            cy.contains('Win 320 coins').should('be.visible') ){
-            console.log('Checked the presence................')
-        }
-        else{
-            console.log('Wrong highlight ............')
-        }
-       
-                
+        cy.get('#reel1 > .notch2').contains('4') & 
+        cy.get('#reel2 > .notch2').contains('4') & 
+        cy.get('#reel3 > .notch2').contains('4') &
+        cy.get('#reel4 > .notch2').contains('4') &           
+        cy.contains('Win 320 coins').should('be.visible')                                   
     })
-    it ('Check if click with test data 44444 adds up 400 to total present score always',() => {
-
-        
+    it ('Check if click with test data 44444 adds up 400 to total present score always',() => {        
         cy.get('#testdata').clear()      
         cy.get('#testdata').type('44444')
         cy.wait(5000)
         cy.get('#spinButton').click()
-        if (cy.get('#reel1 > .notch2').contains('4') & 
-            cy.get('#reel2 > .notch2').contains('4') & 
-            cy.get('#reel3 > .notch2').contains('4') &
-            cy.get('#reel4 > .notch2').contains('4') &
-            cy.get('#reel5 > .notch2').contains('4') &            
-            cy.contains('Win 400 coins').should('be.visible') ){
-            console.log('Checked the presence................')
-        }
-        else{
-            console.log('Wrong highlight ............')
-        }
-        
-        
-                
+        cy.get('#reel1 > .notch2').contains('4') & 
+        cy.get('#reel2 > .notch2').contains('4') & 
+        cy.get('#reel3 > .notch2').contains('4') &
+        cy.get('#reel4 > .notch2').contains('4') &
+        cy.get('#reel5 > .notch2').contains('4') &            
+        cy.contains('Win 400 coins').should('be.visible')                                        
     })
-    it ('Check if click with test data 555 adds up 300 to total present score always',() => {
-
-        
+    it ('Check if click with test data 555 adds up 300 to total present score always',() => {        
         cy.get('#testdata').clear()      
         cy.get('#testdata').type('555')
         cy.wait(5000)
         cy.get('#spinButton').click()
-        if (cy.get('#reel1 > .notch2').contains('5') & 
-            cy.get('#reel2 > .notch2').contains('5') & 
-            cy.get('#reel3 > .notch2').contains('5') &            
-            cy.contains('Win 300 coins').should('be.visible') ){
-            console.log('Checked the presence................')
-        }
-        else{
-            console.log('Wrong highlight ............')
-        }                                
+        cy.get('#reel1 > .notch2').contains('5') & 
+        cy.get('#reel2 > .notch2').contains('5') & 
+        cy.get('#reel3 > .notch2').contains('5') &            
+        cy.contains('Win 300 coins').should('be.visible')                                         
     })
-    it ('Check if click with test data 5555 adds up 400 to total present score always',() => {
-
-        
+    it ('Check if click with test data 5555 adds up 400 to total present score always',() => {        
         cy.get('#testdata').clear()      
         cy.get('#testdata').type('5555')
         cy.wait(5000)
         cy.get('#spinButton').click()
-        if (cy.get('#reel1 > .notch2').contains('5') & 
-            cy.get('#reel2 > .notch2').contains('5') & 
-            cy.get('#reel3 > .notch2').contains('5') & 
-            cy.get('#reel4 > .notch2').contains('5') & 
-            cy.contains('Win 400 coins').should('be.visible')){
-                console.log('Checked the presence................')
-            }
-        
-
-        else{
-            console.log('Wrong highlight ............')
-        }    
-                
+        cy.get('#reel1 > .notch2').contains('5') & 
+        cy.get('#reel2 > .notch2').contains('5') & 
+        cy.get('#reel3 > .notch2').contains('5') & 
+        cy.get('#reel4 > .notch2').contains('5') & 
+        cy.contains('Win 400 coins').should('be.visible')                
     })
     it ('Check if click with test data 55555 adds up 500 to total present score always',() => {
-
         cy.get('#testdata').clear()      
         cy.get('#testdata').type('55555')
         cy.wait(5000)
         cy.get('#spinButton').click()    
-        if (cy.get('#reel1 > .notch2').contains('5') & 
-            cy.get('#reel2 > .notch2').contains('5') & 
-            cy.get('#reel3 > .notch2').contains('5') & 
-            cy.get('#reel4 > .notch2').contains('5') & 
-            cy.get('#reel5 > .notch2').contains('5') &            
-            cy.contains('Win 500 coins').should('be.visible') ){
-            console.log('Checked the presence................')
-        }
-        else{
-            console.log('Wrong highlight ............')
-        }                      
-                
+        cy.get('#reel1 > .notch2').contains('5') & 
+        cy.get('#reel2 > .notch2').contains('5') & 
+        cy.get('#reel3 > .notch2').contains('5') & 
+        cy.get('#reel4 > .notch2').contains('5') & 
+        cy.get('#reel5 > .notch2').contains('5') &            
+        cy.contains('Win 500 coins').should('be.visible')                                               
     })
    
 })
